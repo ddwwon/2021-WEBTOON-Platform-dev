267f6cadc833e7bf7ef7d088b6ef1b642963c626
ouoofo <93857626+ouoofo@users.noreply.github.com>
2021-11-17 14:03:23 +0900

    2021-11-17 (접속 요일에 따른 메인 화면 세팅)
    현주 / 접속 요일에 따른 메인 화면 세팅

--- JS/startPage.js
+++ JS/startPage.js
@@ -21,5 +21,32 @@ const slideShow = () => {
 
 //시작 화면에서 2초 후 메인화면으로 이동
 setTimeout(() => {
-    location.href = "./Components/main.html";
+    setMainforweekDay();
 }, 2000);
+
+
+//접속 요일을 얻는 함수
+function getWeekDayToday(){
+    let now = new Date();
+    const weekDay = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri','Sat'];
+    let week = weekDay[now.getDay()];
+    return week;
+}
+//접속 요일에 따른 메인 화면 세팅
+function setMainforweekDay(){
+    if( getWeekDayToday()== 'Sun' )
+    {location.replace('./Components/일요웹툰/sunWebToonPage.html')}
+    else if( getWeekDayToday() == 'Mon' )
+    {location.replace("./Components/월요웹툰/monWebToonPage.html")}
+    else if( getWeekDayToday() == 'Tue' )
+    {location.replace("./Components/화요웹툰/tueWebToonPage.html")}
+    else if( getWeekDayToday() == 'Wed' )
+    {location.replace("./Components/수요웹툰/wednsWebToonPage.html")}
+    else if( getWeekDayToday() == 'Thu' )
+    {location.replace("./Components/목요웹툰/thursWebToonPage.html")}
+    else if( getWeekDayToday() == 'Fri' )
+    {location.replace("./Components/금요웹툰/friWebToonPage.html")}
+    else if( getWeekDayToday() == 'Sat' )
+    {location.replace("./Components/토요웹툰/saturWebToonPage.html")}
+
+}
