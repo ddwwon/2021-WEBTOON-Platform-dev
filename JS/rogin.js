6d25083d2e5e85222f8c1fcc57750d47e62badf4
ouoofo <93857626+ouoofo@users.noreply.github.com>
2021-11-17 18:51:51 +0900

    2021-11-17 (상단바 및 사이드바 완료)
    (상단바 및 사이드바 완료)

--- JS/rogin.js
+++ JS/rogin.js
@@ -8,12 +8,39 @@ sideBarbtn.addEventListener("click", function () {
     section.style.left = 0;
 });
   
-// 포커스 아웃 시 사이드 메뉴 사라짐, 사이드바 버튼 생성
+// 클릭 시 사이드 메뉴 사라짐, 사이드바 버튼 생성
 closesideBarBtn.addEventListener("click", function () {
     section.style.left = "-100%";
 });
-  
 
+//접속 요일을 얻는 함수
+function getWeekDayToday(){
+    let now = new Date();
+    const weekDay = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri','Sat'];
+    let week = weekDay[now.getDay()];
+    return week;
+}
+
+//접속 요일에 따른 메인 화면 세팅
+function setMainforweekDay(){
+    if( getWeekDayToday()== 'Sun' )
+    {location.href = '../Components/일요웹툰/sunWebToonPage.html'}
+    else if( getWeekDayToday() == 'Mon' )
+    {location.href = "../Components/월요웹툰/monWebToonPage.html"}
+    else if( getWeekDayToday() == 'Tue' )
+    {location.href = "../Components/화요웹툰/tueWebToonPage.html"}
+    else if( getWeekDayToday() == 'Wed' )
+    {location.href = "../Components/수요웹툰/wednsWebToonPage.html"}
+    else if( getWeekDayToday() == 'Thu' )
+    {location.href = "../Components/목요웹툰/thursWebToonPage.html"}
+    else if( getWeekDayToday() == 'Fri' )
+    {location.href = "../Components/금요웹툰/friWebToonPage.html"}
+    else if( getWeekDayToday() == 'Sat' )
+    {location.href = "../Components/토요웹툰/saturWebToonPage.html"}
+
+}
+  
+// 로그인
 function Rogin(){
     const id =document.querySelector('#roginIdInput');
     const pw =document.querySelector('#roginPasswordInput');
