add36406a6467b1d0c49b0432e3673dc10c27bdb
ouoofo <93857626+ouoofo@users.noreply.github.com>
2021-11-17 17:01:36 +0900

    2021-11-17( sidebar 닫는 버튼 구현)
    ( sidebar 닫는 버튼 구현)

--- JS/rogin.js
+++ JS/rogin.js
@@ -1,3 +1,19 @@
+const sideBarbtn = document.querySelector(".sideBar");
+const closesideBarBtn = document.querySelector(".closesideBar");
+const section = document.querySelector(".moreInfo-icon");
+const label = document.querySelector(".moreInfo");
+
+// 클릭 시 사이드 메뉴 생성, 사이드버튼 사라짐
+sideBarbtn.addEventListener("click", function () {
+    section.style.left = 0;
+});
+  
+// 포커스 아웃 시 사이드 메뉴 사라짐, 사이드바 버튼 생성
+closesideBarBtn.addEventListener("click", function () {
+    section.style.left = "-100%";
+});
+  
+
 function Rogin(){
     const id =document.querySelector('#roginIdInput');
     const pw =document.querySelector('#roginPasswordInput');
