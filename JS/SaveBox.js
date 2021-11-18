7668bf82919e468c55b86e4a9da71f5be9cd1416
dowon <70639119+ddwwon@users.noreply.github.com>
2021-11-18 22:04:36 +0900

    2021/11/18 설정화면 완료, 보관함 수정중
    보관함 알람설정 및 버튼 클릭시 이벤트 오류, 수정 필요

--- JS/SaveBox.js
+++ JS/SaveBox.js
@@ -1,8 +1,45 @@
 const sideBarbtn = document.querySelector(".sideBar");
 const section = document.querySelector(".moreInfo-icon");
 const label = document.querySelector(".moreInfo");
-const btn = document.querySelector(".btn");
-const thumbNail = document.querySelector(".thumbNail");
+const alarm_btn = document.querySelector(".btn");
+let flag = 0;
+
+// const popoverTriggerList = [].slice.call(
+//   document.querySelectorAll('[data-bs-toggle="popover"]')
+// );
+// const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+//   return new bootstrap.Popover(popoverTriggerEl);
+// });
+
+// 알림 설정 버튼 클릭시
+const btn_click = () => {
+  const alarm_btn = document.querySelector(".btn");
+  const true_false = alarm_btn.getAttribute("checked");
+  console.log("checked", true_false);
+  if (flag === 0) {
+    console.log("gi");
+    alert("알림 설정이 완료되었습니다");
+    alarm_btn.style.backgroundColor = "#454ade";
+    alarm_btn.style.color = "#fff";
+    console.log("ggi");
+    return (flag = 1);
+  } else if (flag === 1) {
+    alarm_btn.style.backgroundColor = "transparent";
+    alarm_btn.style.color = "#1b1f3b";
+    alert("알림 설정이 해제되었습니다.");
+    return (flag = 0);
+  }
+};
+
+const side_btn_none = () => {
+  section.style.left = 0;
+  label.style.opacity = 0;
+};
+
+// alarm_btn.addEventListener("click", function () {
+//   alarm_btn.style.backgroundColor = "#454ade";
+//   alarm_btn.style.color = "#fff";
+// });
 
 // 클릭 시 사이드 메뉴 생성, 사이드버튼 사라짐
 sideBarbtn.addEventListener("click", function () {
@@ -14,19 +51,4 @@ sideBarbtn.addEventListener("click", function () {
 sideBarbtn.addEventListener("blur", function () {
   section.style.left = "-100%";
   label.style.opacity = 1;
-});
-
-const btn_click = () => {
-  btn.style.backgroundColor = "#454ade";
-  btn.style.color = "white";
-};
-
-thumbNail.addEventListener("mouseover", (event) => {
-  console.log("over");
-  thumbNail.style.opacity = 0.3;
-});
-
-thumbNail.addEventListener("mouseout", (event) => {
-  console.log("out");
-  thumbNail.style.opacity = 1;
 });
--- JS/Setting.js
+++ JS/Setting.js
@@ -1,5 +1,43 @@
-// const ch_nickname = document.querySelector(".Setting_changing_nickname");
-// const ch_passward = document.querySelector(".Setting_changing_password");
-// const QandA = document.querySelector(".Setting_Q&A");
-// const secession = document.querySelector(".Setting_secession");
-const Answer = document.querySelector(".answer");
+const sideBarbtn = document.querySelector(".sideBar");
+const section = document.querySelector(".moreInfo-icon");
+const label = document.querySelector(".moreInfo");
+
+// 클릭 시 사이드 메뉴 생성, 사이드버튼 사라짐
+sideBarbtn.addEventListener("click", function () {
+  section.style.left = 0;
+  label.style.opacity = 0;
+});
+
+// 포커스 아웃 시 사이드 메뉴 사라짐, 사이드바 버튼 생성
+sideBarbtn.addEventListener("blur", function () {
+  section.style.left = "-100%";
+  label.style.opacity = 1;
+});
+
+// 탈퇴 버튼 클릭시 알림창
+const secession = () => {
+  alert("탈퇴가 완료되었습니다.");
+};
+
+// 비밀번호 변경시 알림창 및 일치 여부 확인
+const change_password = () => {
+  const pw1 = document.getElementById("change_pw1").value;
+  const pw2 = document.getElementById("change_pw2").value;
+  if (pw1 !== pw2) {
+    alert("비밀번호가 일치 하지 않습니다.");
+  } else {
+    alert("비밀번호가 변경되었습니다.");
+  }
+};
+
+// 닉네임 변경
+const getName = () => {
+  const name = document.getElementById("changingName").value;
+  document.getElementById("name").innerHTML = name;
+  alert("닉네임이 변경되었습니다.");
+};
+
+// 의견 보내기 버튼 클릭시
+const send_comments = () => {
+  alert("의견이 보내졌습니다.");
+};
