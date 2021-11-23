b56d2691ea5f4cb5b4c66e3af672614af566e397
jung0115 <jungim7490@gmail.com>
2021-11-23 17:15:41 +0900

    11.23. (startPage.js 수정)
    startPage.js의 slideShow() 함수 부분 수정

--- JS/startPage.js
+++ JS/startPage.js
@@ -1,21 +1,22 @@
 //시작 화면에서 0.4초마다 아이콘 변경
-let index = 0;
 window.onload = function() {
     slideShow();
 }
 
+let index = 0; //index 변수 선언 및 초기화
 const slideShow = () => {
     let i;
     const image = document.getElementsByClassName("icon-image"); //class명이 icon-image인 dom 참조
     for(i = 0; i < image.length; i++){
         image[i].style.display = "none"; //처음에는 display를 none으로
     }
+    image[index].style.display = "block"; //해당 인덱스의 display는 block으로
+
     index++;
     if(index >  image.length) {
-        index = 1; //인덱스가 초과되면(아이콘이 한 바퀴 돌면) 1로 다시 초기화
+        index = 0; //인덱스가 초과되면(아이콘이 한 바퀴 돌면) 0으로 다시 초기화
     }
-
-    image[index-1].style.display = "block"; //해당 인덱스의 display는 block으로
+    
     setTimeout(slideShow, 400); //함수를 0.4초마다 호출
 }
 
